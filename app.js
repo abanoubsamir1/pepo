7161688666a1842c08ae9cfc6ee90f0664825cfd
abanoubsamir1 <abanoubsamirr@gmail.com>
2016-12-13 20:28:52 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,2 @@
-app.get('/webhook', function(req, res) {
-  if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === <VERIFY_TOKEN>) {
-    console.log("Validating webhook");
-    res.status(200).send(req.query['hub.challenge']);
-  } else {
-    console.error("Failed validation. Make sure the validation tokens match.");
-    res.sendStatus(403);          
-  }  
-});
+<?php
+print_r($_GET["hub.challenge"]};
